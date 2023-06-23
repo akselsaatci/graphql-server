4226f30cb5f0ccc63e39df85db92477b3990b85a
akselsaatci <aksel.saatci1@gmail.com>
2023-06-23 23:47:37 +0300

    added gql yoga

---
+++ src/app/api/graphql/route.ts
@@ -0,0 +1,17 @@
+import { createServer } from "node:http";
+import { createYoga } from "graphql-yoga";
+import { schema } from "@/../schema/schema";
+
+// Create a Yoga instance with a GraphQL schema.
+const { handleRequest } = createYoga({
+  schema: schema,
+  logging: "debug",
+  graphiql: true,
+
+  fetchAPI: {
+    Response,
+    Request,
+  },
+});
+
+export { handleRequest as GET, handleRequest as POST };
